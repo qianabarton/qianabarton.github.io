80998a38053b769313b9481f0100c044276cbf6a
QIANA <barton.qiana@gmail.com>
2020-07-02 04:12:19 -0700

    deploy

--- src/App.jsx
+++ src/App.jsx
@@ -27,7 +27,7 @@ class App extends Component {
             <Router>
                 <Switch>
                     <Route exact path="/" component={Home}/>
-                    <Route exact path="/projects" component={Projects}/>
+                    <Route path={process.env.PUBLIC_URL + '/projects'} component={Projects}/>
                     <Route exact path="/contact" component={Contact}/>
 
                     <Route exact path="/projects/swiftui" component={SwiftUI}/>
